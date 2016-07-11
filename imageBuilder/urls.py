9926aab6e7ae56508c0011a2ca8d34c8ed5fe306
Chris Lydic <chris@kingdomofchris.com>
2016-07-10 23:10:26 -0400

    Added views to imageBuilder app, added random word generator for urls

--- imageBuilder/urls.py
+++ imageBuilder/urls.py
@@ -5,5 +5,6 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
+    url( r'^new_image/$', views.createImage, name='createImage' ),
     url( r'^(\d+)/$', views.viewImage, name='viewImage' ),
 ]
--- imageBuilder/views.py
+++ imageBuilder/views.py
@@ -1,3 +1,54 @@
 from django.shortcuts import render
+from django.shortcuts import get_object_or_404
+from django.http import HttpResponseRedirect
+from .models import Image
 
-# Create your views here.
+
+def home( request ):
+    return render( request, 'index.html' )
+
+
+def createImage( request ):
+    imgId = makeId()
+
+    newImage = Image(
+        id=imgId,
+        # image=image,
+    )
+    newImage.save()
+
+    return HttpResponseRedirect( '/img/' + imgId + '/' )
+
+
+def viewImage( request, imgId ):
+    imgPath = get_object_or_404( Image, imgId )
+    return render( request, 'image.html', { imgPath: imgPath } )
+
+
+def makeId():
+    with open( 'pieces.txt' ) as file:
+        newId = makeWords( 3, file )
+
+        while len( newId ) < 40 and Image.objects.filter( id=newId ):
+            newId = makeWords( 3, file )
+
+    return newId
+
+
+# generate random ids for images
+def makeWords( len, file ):
+    pieces = set()
+    word = ''
+
+    for line in file:
+        line = line.strip()
+        if line == '':
+            continue
+        else:
+            pieces.add(line)
+
+    while len > 0:
+        word = word + pieces.pop()
+        len -= 1
+
+    return word.capitalize()
